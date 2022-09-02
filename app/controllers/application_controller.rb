871b80f713b64dfc6cd39bce6c06c9a187d375f4
Mark Soper <marksoper@gmail.com>
2022-09-02 13:50:15 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "SqoopQA - Ruby on Azure App Service on Linux!"
+    render html: "Sqoop - Ruby on Azure App Service on Linux!"
   end
 end
